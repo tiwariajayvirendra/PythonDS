901d598447fafb3936ef2d7eb36c7d01ff0aab02
Jigoogle <allentripathi.2529@gmail.com>
2026-02-05 10:13:18 +0530

    Dynamic and Static Programing

--- Variable.py
+++ Variable.py
@@ -1,2 +1,17 @@
 name = 'Nitish Rajpoot'
-print(name)
+# print(name)
+#No Variable Declaration 
+
+#Dynamic Typing - it will autometicaly understand what type of Data type or Variable type
+# Static Typing - In other Language We will se C/ C++ / JAVA in this we will See Static Typing 
+
+#Identifires and Key-words
+
+
+
+
+
+
+
+
+
