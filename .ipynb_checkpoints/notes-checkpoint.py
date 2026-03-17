1f6feb0e3562b5b72c6de6843a6d56a838fd44f5
Jigoogle <ajaytripathi821@gmail.com>
2026-03-17 15:42:49 +0530

    Some List Operations

---
+++ .ipynb_checkpoints/notes-checkpoint.py
@@ -0,0 +1,10 @@
+#create a function and take 2 paramiter 
+#create a function and take 2 paramiter 
+def sub_mult(a,b):
+    sub=a-b
+    mult=a*b
+    div=a/b
+    add=a+b
+    mod=a%b
+    print(sub,mult,div,add,mod)
+
